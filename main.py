6026da0eef493d407bd6a8d8b9e5bdf24ebf85a4
kavi-24 <--replace-all>
2023-07-06 20:27:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import oracledb
 import datetime
-# import prettytable
 
 oracledb.init_oracle_client()
 
@@ -14,7 +13,6 @@ dsn = f"{username}/{password}@{host}:{port}/{service_name}"
 connection = oracledb.connect(dsn=dsn)
 connection.autocommit = True
 
-# Function to add a book
 def add_book():
     cursor = connection.cursor()
     title = input("Enter book title: ")
@@ -29,7 +27,6 @@ def add_book():
     connection.commit()
     print("Book added successfully.")
 
-# Function to add a member
 def add_member():
     cursor = connection.cursor()
     name = input("Enter member name: ")
@@ -42,7 +39,6 @@ def add_member():
     connection.commit()
     print("Member added successfully.")
 
-# Function to add a publisher
 def add_publisher():
     cursor = connection.cursor()
     name = input("Enter publisher name: ")
@@ -54,13 +50,11 @@ def add_publisher():
     connection.commit()
     print("Publisher added successfully.")
 
-# Function to issue a book
 def issue_book():
     cursor = connection.cursor()
     book_id = int(input("Enter book ID: "))
     member_id = int(input("Enter member ID: "))
 
-    # Check if the book is available
     cursor.execute("SELECT quantity FROM books WHERE bookid = :book_id", book_id=book_id)
     result = cursor.fetchone()
     if result is None:
@@ -72,26 +66,22 @@ def issue_book():
         print("Book is currently not available.")
         return
 
-    # Update the quantity and insert the borrowed record
     cursor.execute("UPDATE books SET quantity = quantity - 1 WHERE bookid = :book_id", book_id=book_id)
     cursor.execute("INSERT INTO borrowed (bookid, duedate, issuedate, membid) VALUES (:book_id, sysdate + 14, sysdate, :member_id)", book_id=book_id, member_id=member_id)
     connection.commit()
     print("Book issued successfully.")
 
-# Function to return a book
 def return_book():
     cursor = connection.cursor()
     book_id = int(input("Enter book ID: "))
     member_id = int(input("Enter member ID: "))
 
-    # Check if the book is borrowed by the member
     cursor.execute("SELECT * FROM borrowed WHERE bookid = :book_id AND membid = :member_id", book_id=book_id, member_id=member_id)
     result = cursor.fetchone()
     if result is None:
         print("No such issued book found.")
         return
 
-    # Calculate the fine
     issued_date = result[3].date()
     return_date = datetime.date.today()
     fine_per_day = 10
@@ -99,13 +89,11 @@ def return_book():
     days_diff = (return_date - issued_date).days
     fine = fine_per_day * days_diff
 
-    # Update the returned column, calculate fine, and increase quantity
     cursor.execute("UPDATE borrowed SET returned = 1, returndate = :return_date, fine = :fine WHERE bookid = :book_id AND membid = :member_id", return_date=return_date, fine=fine, book_id=book_id, member_id=member_id)
     cursor.execute("UPDATE books SET quantity = quantity + 1 WHERE bookid = :book_id", book_id=book_id)
     connection.commit()
     print("Book returned successfully. Fine: Rs.", fine)
 
-# Function to display all books
 def display_books():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM books")
@@ -118,7 +106,6 @@ def display_books():
     for row in result:
         print(f"Book ID: {row[0]}, Title: {row[1]}, Author: {row[2]}, Quantity: {row[3]}, Price: {row[4]}, Publisher ID: {row[5]}")
 
-# Function to display members table
 def display_members():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM member")
@@ -131,7 +118,6 @@ def display_members():
     for row in result:
         print(f"Member ID: {row[0]}, Name: {row[1]}, Email: {row[2]}, Phone Number: {row[3]}")
 
-# Function to display publisher table
 def display_publishers():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM publisher")
@@ -144,7 +130,6 @@ def display_publishers():
     for row in result:
         print(f"Publisher ID: {row[0]}, Name: {row[1]}, Address: {row[2]}")
 
-# Function to display borrowed table
 def display_borrowed():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM borrowed")
@@ -168,7 +153,6 @@ def display_borrowed():
         print(f"Book ID: {book_id}, Title: {book_title}, Member ID: {member_id}, Member Name: {member_name}")
         print(f"Issue Date: {issue_date}, Due Date: {due_date}, Return Date: {return_date}, Fine: {fine}")
 
-# Main menu
 while True:
     print("\nLibrary Management System")
     print("1. Add book")
@@ -207,5 +191,4 @@ while True:
     else:
         print("Invalid choice. Please try again.")
 
-# Close the connection
 connection.close()
